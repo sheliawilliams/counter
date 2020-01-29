3e4cfe39aeb09c390408f014383fae3ade70a8e6
shelia <woman5650@gmainl.com>
2020-01-28 20:02:08 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+function tipCalculator() {
+    let btn = document.getElementById('total')
+    btn.addEventListener('click', function (){
+         let output = document.getElementById('output');
+    let ckAmnt = Number
+    
+    document.getElementById('amount').Value;
+
+    let output = document.getElementById
+    let tipPercent = .15;
+    let tip = checkAmount *
+     tipPercent
+
+    
+    
+
+    }
+
+    let total = tip 
+
+
+
+
+
+
